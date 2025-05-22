f2c9935d4087ce7bcdff2708eba27dbfee8e125c
gsolarim <gabriel.solarim@gmail.com>
2025-05-21 23:23:34 -0500

    Agrega 09_Practica | Ejercicio 1

---
+++ Java/01 Curso de Introducción a Java/scripts/09_Practica/src/Practica.java
@@ -0,0 +1,26 @@
+public class Practica {
+    public static void main(String[] args) {
+
+        /*
+         * ### **Variables**
+         **Ejercicio 1.** Ponte creativo y preséntanos a tu familia con variables,
+         * utiliza todos los tipos y cantidad de variables que puedas, no olvides
+         * las convenciones de nombres Camel Case.
+         * */
+
+        String nameMe = "Gabriel";
+        String nameWife = "Aracelly";
+        String nameSisterInLaw1 = "Paloma";
+        String nameSisterInLaw2 = "Sandra";
+
+        int ageMe = 27;
+        int ageWife = 30;
+        int ageSisterInLaw1 = 33;
+        int ageSisterInLaw2 = 23;
+
+        System.out.println("My name is " + nameMe + " and I have " + ageMe + " years old.");
+        System.out.println("My wife is " + nameWife + " and She has " + ageWife + " years old.");
+        System.out.println("My sister in law is " + nameSisterInLaw1 + " and She has " + ageSisterInLaw1 + " years old.");
+        System.out.println("My other sister in law is " + nameSisterInLaw2 + " and She has " + ageSisterInLaw2 + " years old.");
+    }
+}
